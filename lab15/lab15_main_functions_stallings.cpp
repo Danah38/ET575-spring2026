1ee9cb8511eba61cb1c66dcf03bd78fccb6de537
Danah38 <danah.stallings90@student.qcc.cuny.edu>
2026-03-23 15:37:15 +0000

    lab15, main functions file, functions file, examples 1,2,3,4, and lab exercise

---
+++ lab15/lab15_main_functions_stallings.cpp
@@ -0,0 +1,36 @@
+/*
+Danah Stallings
+March 23, 2026
+Lab 15, Random numbers
+*/
+#include<iostream>
+
+using namespace std;
+
+#include "lab15_functions_stallings.cpp"
+
+int main(){
+    cout<<"\n example 1: randon number "<<endl;
+    cout<<randomnumber()<<endl;
+    
+    cout<<"\n example 2: randon number between 0 and 9 "<<endl;
+    cout<<randon_0_9()<<endl;
+
+    cout<<"\n example 3: randon number between 5 and 12 "<<endl;
+    cout<<randon_5_12()<<endl;
+
+    
+    cout<<"\n example 4: matching numbers "<<endl;
+    int roll1 = 1+rand()%6;
+    cout<<endl<<endl;
+    int roll2 = 1+rand()%6;
+    bool resultmatch = matchdices(roll1,roll2);
+    printdices(roll1, roll2, resultmatch);
+
+    
+    cout<<"\n Lab Exercise:  "<<endl;
+    
+    return 0;
+}
+
+
