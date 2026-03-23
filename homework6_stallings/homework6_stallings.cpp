c215dc6f8ee8b9467eaffc404efb3f0643893fb6
Danah38 <danah.stallings90@student.qcc.cuny.edu>
2026-03-23 04:15:50 +0000

    Homework #6, Question 1 and 2.

--- homework6_stallings/homework6_stallings.cpp
+++ homework6_stallings/homework6_stallings.cpp
@@ -24,7 +24,20 @@ int main(){
     int& ref = var;
     cout<<"ref = "<<&ref<<endl;
 
-    Exercise 2) recursive function 
+    cout<<"\n---- Exercise 2) recursive function : ---- "<<endl; 
+    int rose(int n){
+    if (n<=0){
+        return 1;
+    }
+    else{
+        return (rose(n-1)*n);
+    }
+}
+
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+    cout<<"The final answer is "<<rose(num);
     
 
     return 0;
